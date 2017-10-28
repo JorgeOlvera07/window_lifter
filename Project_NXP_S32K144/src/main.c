ebbf1279e52c1b404b2be4fae047ac05d55032a8
luisarcpicazzo <luis.arc.picazzo@gmail.com>
2017-10-28 15:19:26 -0500

    Adding Code version #3 APP, HAL and MAL Folders fully uploaded

--- Project_NXP_S32K144/src/main.c
+++ Project_NXP_S32K144/src/main.c
@@ -1,5 +1,38 @@
 #include "S32K144.h" /* include peripheral declarations S32K144 */
 
-int main(void){
 
+int main(void) {
+
+
+ for (;;) {
+
+	 if ( /*check buttons and flag*/ ) {
+
+		 
+
+		 while(/*check buttons and flags*/){
+
+
+		 if( /*check button UP, flags UP an valid time press (10 ms)*/){  /* valid button press up */
+			 
+
+
+		         }  /* valid button press up */
+
+		 if( /*check button DOWN, flags DOWN an valid time press (10 ms)*/){/* valid button press down */
+
+
+		 		 } /* valid button pres down */
+		 }
+
+	 }
+	 else { 
+		 /* If button UP was not pushed */
+		 /* If button DOWN was not pushed */
+		 
+
+	 }
+
+ }
 }
+
